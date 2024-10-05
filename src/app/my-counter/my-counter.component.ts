c447b929db67d20e69389aa48956ba12dda68c94
sleepynut <nut.pa@kbtg.tech>
2024-10-05 15:51:50 +0700

    initial commit

---
+++ src/app/my-counter/my-counter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-counter',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-counter.component.html',
+  styleUrl: './my-counter.component.css'
+})
+export class MyCounterComponent {
+
+}
